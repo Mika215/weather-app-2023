f740dab96e9ba56cd98e59cd0f3b29db5ce3a569
Michael <michaeltesfayg@gmail.com>
2023-07-05 10:06:57 +0200

    reactDom replaced with creatRoot for latest version react18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+//rendering upgraded to the latest react version18
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+//this was in  older versions of < react 18 it use to work as follows
+
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
